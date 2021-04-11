18afde192011adbf7c352af1ddb89a25f972dac1
Saman <s.sattari@mydigipay.com>
2021-04-11 20:16:22 +0430

    added toolbar to the splash fragment

--- feature/splash/src/main/java/com/turkurt656/feature/splash/ui/SplashFragment.kt
+++ feature/splash/src/main/java/com/turkurt656/feature/splash/ui/SplashFragment.kt
@@ -1,6 +1,7 @@
 package com.turkurt656.feature.splash.ui
 
 import com.turkurt656.common.core.base.BaseFragment
+import com.turkurt656.common.core.ktx.setSupportActionBar
 import com.turkurt656.feature.splash.R
 import com.turkurt656.feature.splash.databinding.SplashFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -9,6 +10,10 @@ class SplashFragment : BaseFragment<SplashViewModel, SplashFragmentBinding>() {
 
     override val viewModel: SplashViewModel by viewModel()
     override val layoutRes: Int = R.layout.splash_fragment
+
+    override fun everyTimeEvent() {
+        setSupportActionBar(binding?.toolbar)
+    }
 
     override fun hookVariables() {
         binding?.vm = viewModel
