6ca83e70d98984da48717f97ee9ed125a92b16a5
Saman <s.sattari@mydigipay.com>
2021-04-11 13:59:57 +0430

    added searching functionality to the list

--- feature/stocks/src/main/java/com/turkurt656/feature/stocks/ui/list/StocksListViewModel.kt
+++ feature/stocks/src/main/java/com/turkurt656/feature/stocks/ui/list/StocksListViewModel.kt
@@ -1,18 +1,31 @@
 package com.turkurt656.feature.stocks.ui.list
 
+import androidx.lifecycle.MutableLiveData
 import com.turkurt656.common.core.base.BaseViewModel
 import com.turkurt656.common.core.ktx.*
 import com.turkurt656.data.domain.dto.stocks.Period
 import com.turkurt656.data.domain.dto.stocks.Stocks
+import com.turkurt656.data.domain.result.Result
 import com.turkurt656.data.domain.usecase.stocks.GetStocksListUseCase
+import com.turkurt656.library.lifecycle.ktx.combine
 
 class StocksListViewModel(
     private val getStocksListUseCase: GetStocksListUseCase,
 ) : BaseViewModel() {
 
     private val _stocksResult = MutableLiveResult<List<Stocks>>()
-    val stocksResult: LiveResult<List<Stocks>> = _stocksResult
 
+    val searchQuery = MutableLiveData<String>()
+
+    val stocksList = combine(_stocksResult, searchQuery) { stocksResult, searchQuery ->
+        when (stocksResult) {
+            is Result.Success -> {
+                if (searchQuery.isNullOrBlank()) return@combine stocksResult.data
+                return@combine stocksResult.data.filter { it.symbol.contains(searchQuery, true) }
+            }
+            else -> return@combine emptyList()
+        }
+    }
 
     init {
         launch {
